2000f89a1f4b48662586b5df3c8082ff0cfc98ee
tsmx <strs@freenet.de>
2021-02-10 20:30:28 +0100

    Refactoring. Added default durationMs.

--- src/Counter.js
+++ src/Counter.js
@@ -4,9 +4,7 @@ class Counter extends React.Component {
 
     constructor(props) {
         super(props);
-        this.countTo = props.countTo;
         this.range = props.countTo - props.countFrom;
-        this.duration = props.durationMs;
         this.state = { currentValue: props.countFrom };
     }
 
@@ -14,31 +12,35 @@ class Counter extends React.Component {
         // no timer shorter than 50ms (not really visible any way)
         let minTimer = 50;
         // calc step time to show all interediate values
-        let stepTime = Math.abs(Math.floor(this.duration / this.range));
+        let stepTime = Math.abs(Math.floor(this.props.durationMs / this.range));
         // never go below minTimer
         stepTime = Math.max(stepTime, minTimer);
         // get current time and calculate desired end time
         let startTime = new Date().getTime();
-        this.endTime = startTime + this.duration;
-        this.timer = setInterval(() => { this.run(); }, stepTime);
-        this.run();
+        this.endTime = startTime + this.props.durationMs;
+        this.timer = setInterval(() => { this.countUp(); }, stepTime);
+        this.countUp();
     }
 
-    run() {
+    countUp() {
         let now = new Date().getTime();
-        let remaining = Math.max((this.endTime - now) / this.duration, 0);
-        let value = Math.round(this.countTo - (remaining * this.range));
+        let remaining = Math.max((this.endTime - now) / this.props.durationMs, 0);
+        let value = Math.round(this.props.countTo - (remaining * this.range));
         this.setState({ currentValue: value });
-        if (value === this.countTo) {
+        if (value === this.props.countTo) {
             clearInterval(this.timer);
         }
     }
 
 
     render() {
-        return <div>Counter: {this.state.currentValue}</div>;
+        return <span>{this.state.currentValue}</span>;
     }
 
+}
+
+Counter.defaultProps = {
+    durationMs: 400
 }
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const App = () => {
     return (
         <div>
             <div>Hello, React world!</div>
-            <Counter countFrom={0} countTo={123} durationMs={400}/>
+            <div>Counter: <Counter countFrom={0} countTo={123} durationMs={400}/></div>
         </div>
     );
 }
