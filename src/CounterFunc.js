9ab625dd2fa6bfb55e3154d1e05ddc058e70ece8
tsmx <strs@freenet.de>
2021-05-02 20:11:32 +0200

    Functional component improved

--- src/CounterFunc.js
+++ src/CounterFunc.js
@@ -3,31 +3,28 @@ import React, { useState, useEffect, useRef } from 'react';
 const CounterFunc = (props) => {
 
     const [counter, setCounter] = useState(props.countFrom);
-    const timer = useRef(null);
-    const effectProps = useRef(props);
+    const propsRef = useRef(props);
     const minTimer = 50;
 
     useEffect(() => {
-        var range = effectProps.current.countTo - effectProps.current.countFrom;
+        const range = propsRef.current.countTo - propsRef.current.countFrom;
         // calc step time to show all intermediate values, never go below minTimer
-        let stepTime = Math.max(Math.abs(Math.floor(effectProps.current.durationMs / range)), minTimer);
+        const stepTime = Math.max(Math.abs(Math.floor(propsRef.current.durationMs / range)), minTimer);
         // get current time and calculate desired end time
-        let startTime = new Date().getTime();
-        var endTime = startTime + effectProps.current.durationMs;
-        timer.current = setInterval(() => {
-            let now = new Date().getTime();
-            let remaining = Math.max((endTime - now) / effectProps.current.durationMs, 0);
-            let value = Math.round(effectProps.current.countTo - (remaining * range));
+        const endTime = new Date().getTime() + propsRef.current.durationMs;
+        var timer = setInterval(() => {
+            let remaining = Math.max((endTime - new Date().getTime()) / propsRef.current.durationMs, 0);
+            let value = Math.round(propsRef.current.countTo - (remaining * range));
             setCounter(value);
-            if (value === effectProps.current.countTo) {
-                clearInterval(timer.current);
-                timer.current = null;
+            if (value === propsRef.current.countTo) {
+                clearInterval(timer);
+                timer = null;
             }
         }, stepTime);
         // clean-up in case component unmounts before counting-up is done
         return () => {
-            if (timer.current) {
-                clearInterval(timer.current);
+            if (timer) {
+                clearInterval(timer);
             }
         };
     }, []);
