7e2591068276571620a42d63787fa332b64c8b42
tsmx <strs@freenet.de>
2021-05-02 20:34:34 +0200

    Indention of components src files reduced for nicer view in markdown

--- src/Counter.js
+++ src/Counter.js
@@ -2,52 +2,52 @@ import React from 'react';
 
 class Counter extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.range = props.countTo - props.countFrom;
-        this.state = { currentValue: props.countFrom };
-    }
+  constructor(props) {
+    super(props);
+    this.range = props.countTo - props.countFrom;
+    this.state = { currentValue: props.countFrom };
+  }
 
-    componentDidMount() {
-        // no timer shorter than 50ms (not really visible any way)
-        const minTimer = 50;
-        // calc step time to show all interediate values
-        let stepTime = Math.abs(Math.floor(this.props.durationMs / this.range));
-        // never go below minTimer
-        stepTime = Math.max(stepTime, minTimer);
-        // get current time and calculate desired end time
-        const startTime = new Date().getTime();
-        this.endTime = startTime + this.props.durationMs;
-        this.timer = setInterval(() => { this.countUp(); }, stepTime);
-        this.countUp();
-    }
+  componentDidMount() {
+    // no timer shorter than 50ms (not really visible any way)
+    const minTimer = 50;
+    // calc step time to show all interediate values
+    let stepTime = Math.abs(Math.floor(this.props.durationMs / this.range));
+    // never go below minTimer
+    stepTime = Math.max(stepTime, minTimer);
+    // get current time and calculate desired end time
+    const startTime = new Date().getTime();
+    this.endTime = startTime + this.props.durationMs;
+    this.timer = setInterval(() => { this.countUp(); }, stepTime);
+    this.countUp();
+  }
 
-    componentWillUnmount() {
-        if (this.timer) {
-            clearInterval(this.timer);
-        }
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
     }
+  }
 
-    countUp() {
-        const now = new Date().getTime();
-        const remaining = Math.max((this.endTime - now) / this.props.durationMs, 0);
-        const value = Math.round(this.props.countTo - (remaining * this.range));
-        this.setState({ currentValue: value });
-        if (value === this.props.countTo) {
-            clearInterval(this.timer);
-            this.timer = null;
-        }
+  countUp() {
+    const now = new Date().getTime();
+    const remaining = Math.max((this.endTime - now) / this.props.durationMs, 0);
+    const value = Math.round(this.props.countTo - (remaining * this.range));
+    this.setState({ currentValue: value });
+    if (value === this.props.countTo) {
+      clearInterval(this.timer);
+      this.timer = null;
     }
+  }
 
 
-    render() {
-        return <span>{this.state.currentValue}</span>;
-    }
+  render() {
+    return <span>{this.state.currentValue}</span>;
+  }
 
 }
 
 Counter.defaultProps = {
-    durationMs: 400
+  durationMs: 400
 }
 
 export default Counter;
--- src/CounterFunc.js
+++ src/CounterFunc.js
@@ -2,42 +2,42 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const CounterFunc = (props) => {
 
-    const [counter, setCounter] = useState(props.countFrom);
-    const propsRef = useRef(props);
-    const minTimer = 50;
+  const [counter, setCounter] = useState(props.countFrom);
+  const propsRef = useRef(props);
+  const minTimer = 50;
 
-    useEffect(() => {
-        const range = propsRef.current.countTo - propsRef.current.countFrom;
-        // calc step time to show all intermediate values, never go below minTimer
-        const stepTime = Math.max(Math.abs(Math.floor(propsRef.current.durationMs / range)), minTimer);
-        // get current time and calculate desired end time
-        const startTime = new Date().getTime();
-        const endTime = startTime + propsRef.current.durationMs;
-        var timer = setInterval(() => {
-            const now = new Date().getTime();
-            const remaining = Math.max((endTime - now) / propsRef.current.durationMs, 0);
-            const value = Math.round(propsRef.current.countTo - (remaining * range));
-            setCounter(value);
-            if (value === propsRef.current.countTo) {
-                clearInterval(timer);
-                timer = null;
-            }
-        }, stepTime);
-        // clean-up in case component unmounts before counting-up is done
-        return () => {
-            if (timer) {
-                clearInterval(timer);
-            }
-        };
-    }, []);
+  useEffect(() => {
+    const range = propsRef.current.countTo - propsRef.current.countFrom;
+    // calc step time to show all intermediate values, never go below minTimer
+    const stepTime = Math.max(Math.abs(Math.floor(propsRef.current.durationMs / range)), minTimer);
+    // get current time and calculate desired end time
+    const startTime = new Date().getTime();
+    const endTime = startTime + propsRef.current.durationMs;
+    var timer = setInterval(() => {
+      const now = new Date().getTime();
+      const remaining = Math.max((endTime - now) / propsRef.current.durationMs, 0);
+      const value = Math.round(propsRef.current.countTo - (remaining * range));
+      setCounter(value);
+      if (value === propsRef.current.countTo) {
+        clearInterval(timer);
+        timer = null;
+      }
+    }, stepTime);
+    // clean-up in case component unmounts before counting-up is done
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, []);
 
-    return (
-        <span>{counter}</span>
-    );
+  return (
+    <span>{counter}</span>
+  );
 }
 
 CounterFunc.defaultProps = {
-    durationMs: 400
+  durationMs: 400
 }
 
 export default CounterFunc;
